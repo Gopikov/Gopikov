b8bc98a17cd96b7f0af060b0212a279e7107fc95
Gopikov <gopiazure30@gmail.com>
2022-02-10 17:27:31 +0530

    Arthmatic Operations devison

--- ArthimaticOper.ps1
+++ ArthimaticOper.ps1
@@ -3,7 +3,9 @@ $b = 20
 $addtion = $a+$b
 $sub = $a-$b
 $mult = $a*$b
+$dev = $a/$b
 
 write-host "Addtion of $a and  $b is $addtion"
 write-host "Subtraction of $a and  $b is $sub"
-write-host "multiplicationof $a and  $b is $mult"
+write-host "multiplication of $a and  $b is $mult"
+write-host "devision of $a and  $b is $dev"
