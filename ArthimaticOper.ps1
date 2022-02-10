4bf4d01c77d6b5bca2c2cb7a400d2f5a156d145f
Gopikov <gopiazure30@gmail.com>
2022-02-10 17:16:16 +0530

    Arthmatic Operations multiplication

--- ArthimaticOper.ps1
+++ ArthimaticOper.ps1
@@ -2,6 +2,8 @@ $a = 10
 $b = 20
 $addtion = $a+$b
 $sub = $a-$b
+$mult = $a*$b
 
 write-host "Addtion of $a and  $b is $addtion"
 write-host "Subtraction of $a and  $b is $sub"
+write-host "multiplicationof $a and  $b is $mult"
