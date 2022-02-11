f2aa1955b1c82c97482821ae831cc3b34856e5df
Gopikov <gopiazure30@gmail.com>
2022-02-11 22:56:35 +0530

    arthamaticOper

---
+++ ArthimaticOper.ps1
@@ -0,0 +1,13 @@
+$a = 10
+$b = 20
+$addtion = $a+$b
+$sub = $a-$b
+$mult = $a*$b
+$dev = $a/$b
+$moduls = $a%$b
+
+write-host "Addtion of $a and  $b is $addtion"
+write-host "Subtraction of $a and  $b is $sub"
+write-host "multiplication of $a and  $b is $mult"
+write-host "devision of $a and $b is $dev"
+
