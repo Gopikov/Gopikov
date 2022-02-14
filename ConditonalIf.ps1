4f6971ebdf34d652ac3c50d911476639b9e8635e
Gopikov <gopiazure30@gmail.com>
2022-02-14 10:45:59 +0530

    If else Conditions

--- ConditonalIf.ps1
+++ ConditonalIf.ps1
@@ -4,3 +4,13 @@ $b= 10;
 if($a -eq $b){
 	write-host "$a and $b is same ";
 }
+
+
+$c = 23;
+$d = 40;
+
+if($a -eq $b){
+	write-host "$c and $d is same";
+}else{
+	write-host "$c and $d is are not same";
+}
