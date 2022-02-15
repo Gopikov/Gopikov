99f1f12f861ac551d1f039bd710a9cf8dcb27fe8
Gopikov <gopiazure30@gmail.com>
2022-02-15 07:50:11 +0530

    commit logical oper

--- LogicalOpertor.ps1
+++ LogicalOpertor.ps1
@@ -1,14 +1,21 @@
 
 write-host "-and (Logical AND)"
 
-#true and true
-# true and false
-# false and true 
-# fasle and false
+#true and true = true
+# true and false = false 
+# false and true = false
+# fasle and false = false
  
  $a=10  
  $b=20  
 ($a -lt $b) -and ($a -eq 10)
+
+#[boolean]$a1 = true -and true # true & true - true
+#$a2 = $t -and $f
+
+$a1 = 0 -and 0
+write-host "true and true is $a1"
+ 
  
 write-host "-or (Logical OR)"
 
