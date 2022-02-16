5ef6fd1a388932d19307392cf21e005963d5369b
Gopikov <gopiazure30@gmail.com>
2022-02-16 23:24:54 +0530

    commit psloop

--- psloops.ps1
+++ psloops.ps1
@@ -19,3 +19,31 @@ do{
 	 $i=$i+1
  }while ($i -le 10)
  
+ write-host FORLOOP
+ 
+ for ($x=1; $x -lt 10; $x=$x+1){
+	 write-host $x
+ }
+ 
+ $arrcolors= "red","orange","blue","green","white","black"
+ for ($i=0;$i -lt $arrcolors.length;$i++){
+	 $arrcolors[$i]
+ }
+ 
+for ($i=1; $i -le 30; $i++){
+	if($i -le 1){
+	write-host "Even - Odd"
+}$res=$i%2
+if ($res -eq 0){
+	write-host " $i "
+} else {
+	write-host " $i "
+}
+}
+
+write-host "FOReachLOOP"
+
+$array= 1,2,3,4,5,6,7,8,9,10
+foreach($number in $array){
+	write-host $number
+}
